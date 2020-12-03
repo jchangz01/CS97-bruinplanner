0de2071d2b5632f8beee1380b2a19323b975a545
David Zhao <daweiz@ucla.edu>
2020-12-02 21:22:00 -0800

    Added About Page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import '../css/About.css'
+import David from '../images/1.jpg'
+import Nilay from '../images/2.jpg'
+import Justin from '../images/3.jpg'
+import Gaurav from '../images/4.jpg'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendar } from '@fortawesome/free-solid-svg-icons'
 
@@ -7,6 +12,7 @@ export default class About extends React.Component {
     
     render() {
         return (
+
             <div>
                 <header id="header">
                     <div id="header-content">
@@ -19,8 +25,57 @@ export default class About extends React.Component {
                             <a href="/log-in"><button className="header-buttons blue">LOG IN</button></a>
                         </nav>
                     </div>
-                </header>
+                </header> 
+            <div class="about-section">
+                <h2>ABOUT US</h2> 
+                <p>Bulit by Justin Chang, Nilay Shah, Gaurav Kale and David Zhao at UCLA.</p>
+                <p>Courses data provided by <a href="http://api.ucladevx.com/"> DevX at UCLA.</a></p>
             </div>
+            <div class="row">
+                <div class="column">
+                <div class="card">
+                <img id="profile-pic" src={Justin} alt="Justin"/>
+                <div class="container">
+                    <p>Justin Chang</p>
+                    <p>?</p>
+                    <a class="button" href="mailto:nilay.shah19@gmail.com">Contact</a>
+                </div>
+                </div>
+                </div>
+                <div class="column">
+                <div class="card">
+                <img id="profile-pic" src={Nilay} alt="Nilay"/>
+                <div class="container">
+                    <p>Nilay Shah</p>
+                    <p>nilay.shah19@gmail.com</p>
+                    <a class="button" href="mailto:nilay.shah19@gmail.com">Contact</a>
+                </div>
+                </div>
+                </div>
+                <div class="column">
+                <div class="card">
+                <img id="profile-pic" src={Gaurav} alt="Gaurav"/>
+                <div class="container">
+                    <p>Gaurav Kale</p>
+                    <p>gauravkale100@gmail.com</p>
+                    <a class="button" href="mailto:gauravkale100@gmail.com">Contact</a>
+                </div>
+                </div>
+                </div>
+                <div class="column">
+                <div class="card">
+                <img id="profile-pic" src={David} alt="David"/>
+                <div class="container">
+                    <p>David Zhao</p>
+                    <p>daweiz@ucla.edu</p>
+                    <a class="button" href="mailto:daweiz@ucla.edu">Contact</a>
+                    <p>  </p>
+                </div>
+                </div>
+                </div>
+            </div>
+            </div>
+
         )
     }
 
