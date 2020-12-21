d37578a6c8abeffdbbde5a3fffe847c4518ad78c
Gaurav Kale <gauravkale100@gmail.com>
2020-12-20 17:01:24 -0800

    FIXING up file names

--- client/src/requirements/ChemE_Reqs.js
+++ client/src/requirements/ChemE_Reqs.js
@@ -5,25 +5,26 @@ import React from 'react'
 
 
 
-export default class CivilEng extends React.Component {
+export default class ChemEng extends React.Component {
   render(){
     return(
       <div>
 
-      <h1>Civil Engineering Major Requirements</h1>
+      <h1>Chemical Engineering Major Requirements</h1>
 
       <h2>Lower Division Requirements</h2>
       <ul>
-        <li>Chemistry and Biochemistry 20A, 20B, 20L, Com Sci 31</li>
+        <li>Chemistry and Biochemistry 20A, 20B, 20L,30A,30AL,30B, Chem Eng 10</li>
         <li>MATH: 31A, 31B, 32A, 32B, 33A, 33B</li>
-        <li>PHYSICS: 1A, 1B, 1C,4AL, LIFE SCIENCE (1)</li>
+        <li>PHYSICS: 1A, 1B, 1C,4AL</li>
       </ul>  
 
     <h2>Upper Division Requirements</h2>
     <ul>
-      <li>MAE 105A, CEE 91, MAE 102,103,C104,108,110,120,135A,150,153,190</li>
-      <li>5 Field Elective Courses: Materials, Environmental, Geotechnical</li>
-      <li>2 Design courses (Capstone)</li>
+      <li>Chemical Engineering 45, 100, 101A, 101B, 101C, 102A, 102B, 103, 104A, 104B, 106, 107, 109;</li>
+      <li>2 Capstone Courses: Chem Eng 108A,B</li>
+      <li>2 Elective Courses Chem Eng </li>
+      <li>3 Courses: HSEAS TECH BREADTH</li>
     </ul>  
 
     <h2>Writing and General Education Requirements</h2>
